86e6c2d11f9ee937e1314353807e354ff01e7a6c
[dyad] <avidacomsucesso@gmail.com>
2026-01-09 15:26:06 +0000

    [dyad] Reimplementando o Google Tag através de um componente React - wrote 3 file(s)

---
+++ src/components/GoogleTag.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+
+const GOOGLE_TAG_ID = 'AW-17858320955';
+
+const GoogleTag: React.FC = () => {
+  useEffect(() => {
+    const existingScript = document.querySelector(`script[src="https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}"]`);
+    if (existingScript) {
+      return;
+    }
+
+    const gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`;
+    document.head.appendChild(gtagScript);
+
+    const gtagInlineScript = document.createElement('script');
+    gtagInlineScript.id = 'gtag-inline-script';
+    gtagInlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${GOOGLE_TAG_ID}');
+    `;
+    document.head.appendChild(gtagInlineScript);
+
+    return () => {
+      // Cleanup on component unmount
+      if (document.head.contains(gtagScript)) {
+        document.head.removeChild(gtagScript);
+      }
+      const inlineScript = document.getElementById('gtag-inline-script');
+      if (inlineScript && document.head.contains(inlineScript)) {
+        document.head.removeChild(inlineScript);
+      }
+    };
+  }, []);
+
+  return null;
+};
+
+export default GoogleTag;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
+import GoogleTag from './GoogleTag';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,7 +10,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
+      <GoogleTag />
+      <ScrollToTop />
       <Header />
       <main className="flex-grow">
         {children}
