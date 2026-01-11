776a2fabcfb5bfc582589a90d8f74a3788f1fd5a
[dyad] <avidacomsucesso@gmail.com>
2026-01-11 14:15:56 +0000

    [dyad] Adicionando a nova tag do Google Analytics - wrote 1 file(s)

--- src/components/GoogleTag.tsx
+++ src/components/GoogleTag.tsx
@@ -1,39 +1,30 @@
 import React, { useEffect } from 'react';
 
-const GOOGLE_TAG_ID = 'AW-17823816531';
+const GOOGLE_TAG_IDS = ['AW-17858320955', 'AW-17823816531'];
 
 const GoogleTag: React.FC = () => {
   useEffect(() => {
-    const existingScript = document.querySelector(`script[src="https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}"]`);
-    if (existingScript) {
-      return;
-    }
-
-    const gtagScript = document.createElement('script');
-    gtagScript.async = true;
-    gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`;
-    document.head.appendChild(gtagScript);
-
-    const gtagInlineScript = document.createElement('script');
-    gtagInlineScript.id = 'gtag-inline-script';
-    gtagInlineScript.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${GOOGLE_TAG_ID}');
-    `;
-    document.head.appendChild(gtagInlineScript);
-
-    return () => {
-      // Cleanup on component unmount
-      if (document.head.contains(gtagScript)) {
-        document.head.removeChild(gtagScript);
+    GOOGLE_TAG_IDS.forEach(id => {
+      const existingScript = document.querySelector(`script[src="https://www.googletagmanager.com/gtag/js?id=${id}"]`);
+      if (existingScript) {
+        return;
       }
-      const inlineScript = document.getElementById('gtag-inline-script');
-      if (inlineScript && document.head.contains(inlineScript)) {
-        document.head.removeChild(inlineScript);
-      }
-    };
+
+      const gtagScript = document.createElement('script');
+      gtagScript.async = true;
+      gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
+      document.head.appendChild(gtagScript);
+
+      const gtagInlineScript = document.createElement('script');
+      gtagInlineScript.id = `gtag-inline-script-${id}`;
+      gtagInlineScript.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${id}');
+      `;
+      document.head.appendChild(gtagInlineScript);
+    });
   }, []);
 
   return null;
